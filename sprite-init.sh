6f6144899fd8c143c4d3a0546ac09cfc0caabc42
Sprite <noreply@sprites.dev>
2026-05-17 12:33:16 +0000

    save

--- sprite-init.sh
+++ sprite-init.sh
@@ -6,3 +6,5 @@ sudo apt install -y elixir
 cp dotemacs ~/.emacs
 cp sprite-dotmybashrc ~/.mybashrc
 echo source .mybashrc >> ~/.profile
+
+git config user.name "CK Tan" && git config user.email "cktanx@gmail.com"

185a5f96a5f1026a40dbbf4acb38b0aa822f3780
Sprite <noreply@sprites.dev>
2026-05-28 03:36:16 +0000

    save

--- sprite-init.sh
+++ sprite-init.sh
@@ -9,4 +9,7 @@ cp sprite-dotmybashrc ~/.mybashrc
 echo source .mybashrc >> ~/.profile
 
 git config user.name "CK Tan" && git config user.email "cktanx@gmail.com"
+git config pull.rebase true
 
+cp sprite-idle-killer.py ~/.local/bin/sprite-idle-killer.py
+python3 ~/.local/bin/sprite-idle-killer.py
